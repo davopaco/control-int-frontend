b8e011b1d49a2e9483a94000dcab2834bcb061ad
Santiago Cárdenas <107714070+davopaco@users.noreply.github.com>
2026-02-06 17:22:18 -0500

    feat: Visualizar imágenes en carrusel para los productos detallados
    Las imágenes se obtienen de un endpoint al cuál se accede para obtener el contenido en formato base64. Luego este es renderizado dentro del carrusel.

--- productmanager/src/app/products-detail/products-detail.ts
+++ productmanager/src/app/products-detail/products-detail.ts
@@ -4,15 +4,18 @@ import { ActivatedRoute } from '@angular/router';
 import { MessageService } from 'primeng/api';
 import ProductDto from '../../dtos/ProductDto';
 import { CardModule } from 'primeng/card';
+import { CarouselModule } from 'primeng/carousel';
+import ImagenDto from '../../dtos/ImagenDto';
 
 @Component({
   selector: 'app-products-detail',
-  imports: [CardModule],
+  imports: [CardModule, CarouselModule],
   templateUrl: './products-detail.html',
   styleUrl: './products-detail.css',
 })
 export class ProductsDetail {
   public product!: ProductDto;
+  public images: ImagenDto[] = [];
 
   constructor(
     private api: ApiService,
@@ -23,6 +26,7 @@ export class ProductsDetail {
 
   ngOnInit() {
     this.getProductDetailed();
+    this.getImagesForProduct();
   }
 
   //Obtener la información del producto detallado
@@ -31,6 +35,23 @@ export class ProductsDetail {
     this.api.get<ProductDto>(`products/${responseId}`).subscribe({
       next: (data: ProductDto) => {
         this.product = data;
+        this.cdr.detectChanges();
+      },
+      error: (data: any) => {
+        this.msg.add({
+          severity: 'error',
+          summary: 'An error occurred',
+          detail: data.error,
+        });
+      },
+    });
+  }
+
+  getImagesForProduct() {
+    const responseId = this.route.snapshot.paramMap.get('id');
+    this.api.get<ImagenDto[]>(`images/${responseId}`).subscribe({
+      next: (data: ImagenDto[]) => {
+        this.images = data;
         this.cdr.detectChanges();
       },
       error: (data: any) => {
