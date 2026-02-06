34ffd3c3d4d606325ed129d1b5c1a198a0199244
Santiago Cárdenas <107714070+davopaco@users.noreply.github.com>
2026-02-06 18:03:32 -0500

    feat: subir imágenes
    Permite subir imágenes a partir de múltiples de estas que se escojan.

--- productmanager/src/app/products-detail/products-detail.ts
+++ productmanager/src/app/products-detail/products-detail.ts
@@ -6,23 +6,28 @@ import ProductDto from '../../dtos/ProductDto';
 import { CardModule } from 'primeng/card';
 import { CarouselModule } from 'primeng/carousel';
 import ImagenDto from '../../dtos/ImagenDto';
+import { FileUploadEvent, FileUploadHandlerEvent, FileUploadModule } from 'primeng/fileupload';
+import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-products-detail',
-  imports: [CardModule, CarouselModule],
+  imports: [CardModule, CarouselModule, FileUploadModule, ButtonModule],
   templateUrl: './products-detail.html',
   styleUrl: './products-detail.css',
 })
 export class ProductsDetail {
   public product!: ProductDto;
   public images: ImagenDto[] = [];
+  public productId!: string | null;
 
   constructor(
     private api: ApiService,
     private route: ActivatedRoute,
     private cdr: ChangeDetectorRef,
     private msg: MessageService,
-  ) {}
+  ) {
+    this.productId = this.route.snapshot.paramMap.get('id');
+  }
 
   ngOnInit() {
     this.getProductDetailed();
@@ -31,8 +36,7 @@ export class ProductsDetail {
 
   //Obtener la información del producto detallado
   getProductDetailed() {
-    const responseId = this.route.snapshot.paramMap.get('id');
-    this.api.get<ProductDto>(`products/${responseId}`).subscribe({
+    this.api.get<ProductDto>(`products/${this.productId}`).subscribe({
       next: (data: ProductDto) => {
         this.product = data;
         this.cdr.detectChanges();
@@ -48,10 +52,35 @@ export class ProductsDetail {
   }
 
   getImagesForProduct() {
-    const responseId = this.route.snapshot.paramMap.get('id');
-    this.api.get<ImagenDto[]>(`images/${responseId}`).subscribe({
+    this.api.get<ImagenDto[]>(`images/${this.productId}`).subscribe({
       next: (data: ImagenDto[]) => {
         this.images = data;
+        this.cdr.detectChanges();
+      },
+      error: (data: any) => {
+        this.msg.add({
+          severity: 'error',
+          summary: 'An error occurred',
+          detail: data.error,
+        });
+      },
+    });
+  }
+
+  async onUpload(event: FileUploadHandlerEvent) {
+    const imagesToUpload = event.files;
+    const reader = new FileReader();
+    imagesToUpload.forEach((image) => {
+      reader.readAsDataURL(image);
+      reader.onload = () => this.createImage(reader.result!.toString());
+    });
+  }
+
+  createImage(content: string) {
+    const body = { content: content, productId: this.productId };
+    this.api.post('images', body).subscribe({
+      next: (data: any) => {
+        this.getImagesForProduct();
         this.cdr.detectChanges();
       },
       error: (data: any) => {
